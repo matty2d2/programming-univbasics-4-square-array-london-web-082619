c6d1b986e1046e34262dabc77a1290213fdb535f
Matthew Dias Prada <matthew.dias.prada@gmail.com>
2019-08-17 19:53:00 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  new_array = []                       #creates new_array to store the squared numbers
+  counter = 0                          #intializes counter
+  while array[counter] do              #loops across all elements in array
+    new_array.push(array[counter]**2)  #adds each squared number to end of the new_array
+    counter += 1                       #increments counter
+  end
+  new_array                            #returns the array with squared numbers
 end
